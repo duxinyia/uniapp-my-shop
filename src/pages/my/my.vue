4badf45b0fa3cbc9bf288ab8f23c493da48f11c2
duxinyi <2580191658@qq.com>
2023-10-10 20:19:00 +0800

    navbar完成

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
-import '@/utils/http'
+import { http } from '@/utils/http'
 
 const memberStore = useMemberStore()
-const getData = () => {
-  uni.request({
+const getData = async () => {
+  const res = await http<string[]>({
     method: 'GET',
     url: '/home/banner',
+    header: {},
   })
 }
 </script>
